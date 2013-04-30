b157fe3390b078c5ddd852167e9bdcd1ca899d39
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2013-04-30 21:36:48 +0200

    the vmod learned how to handle content types
    Using libmagic on this one. This lib is truly awesome, despite a poor
    API. The quality of the Content-Type header is dependant of the quality
    of the magicdb used, which could be parameterizable.

--- src/vmod_fsdirector.c
+++ src/vmod_fsdirector.c
@@ -42,6 +42,8 @@
 #include <unistd.h>
 #include <netinet/in.h> 
 
+#include <magic.h>
+
 #include "vre.h"
 #include "vrt.h"
 #include "vas.h"
@@ -83,6 +85,8 @@ struct vmod_fsdirector_file_system {
         struct sockaddr_in       sockaddr;
         pthread_t                tp;
         struct worker            *wrk;
+        struct http_conn         htc;
+        magic_t                  magic_cookie;
 };
 
 static void
@@ -107,11 +111,8 @@ prepare_answer(struct http_conn *htc, int status)
 }
 
 static void
-prepare_body(struct http_conn *htc, const char *content_type)
+prepare_body(struct http_conn *htc)
 {
-	if (content_type) {
-		dprintf(htc->fd, "Content-Type: %s\r\n", content_type);
-	}
 	dprintf(htc->fd, "\r\n");
 }
 
@@ -136,11 +137,26 @@ handle_stat_error(struct http_conn *htc, int err) {
 	}
 
 	prepare_answer(htc, status);
-	prepare_body(htc, NULL);
+	prepare_body(htc);
 }
 
 static void
-send_response(struct http_conn *htc, struct stat *stat_buf, const char *path)
+add_content_type(struct vmod_fsdirector_file_system *fs, const char *path)
+{
+	const char *mime = magic_file(fs->magic_cookie, path);
+
+	// XXX how to free a string from magic_file or magic_error ?
+	if (mime == NULL) {
+		perror(magic_error(fs->magic_cookie));
+		return;
+	}
+
+	dprintf(fs->htc.fd, "Content-Type: %s\r\n", mime);
+}
+
+static void
+send_response(struct vmod_fsdirector_file_system *fs, struct stat *stat_buf,
+    const char *path)
 {
 	int fd;
 	off_t offset = 0;
@@ -152,12 +168,13 @@ send_response(struct http_conn *htc, struct stat *stat_buf, const char *path)
 	// TODO handle failure
 	assert(fd > 0);
 
-	prepare_answer(htc, 200);
-	dprintf(htc->fd, "Content-Length: %lu\r\n", stat_buf->st_size);
-	prepare_body(htc, NULL); // XXX handle content type ?
+	prepare_answer(&fs->htc, 200);
+	dprintf(fs->htc.fd, "Content-Length: %lu\r\n", stat_buf->st_size);
+	add_content_type(fs, path);
+	prepare_body(&fs->htc);
 
 	while (remaining > 0) {
-		written = sendfile(htc->fd, fd, &offset, remaining);
+		written = sendfile(fs->htc.fd, fd, &offset, remaining);
 		if (written < 0) {
 			perror("sendfile");
 			break;
@@ -170,30 +187,31 @@ send_response(struct http_conn *htc, struct stat *stat_buf, const char *path)
 }
 
 static void
-answer_file(struct http_conn *htc, struct stat *stat_buf, const char *path)
+answer_file(struct vmod_fsdirector_file_system *fs, struct stat *stat_buf,
+    const char *path)
 {
 	mode_t mode = stat_buf->st_mode;
 
 	if (S_ISREG(mode)) {
 		if (stat_buf->st_size) {
-			send_response(htc, stat_buf, path);
+			send_response(fs, stat_buf, path);
 		}
 		else {
-			prepare_answer(htc, 204);
-			prepare_body(htc, NULL);
+			prepare_answer(&fs->htc, 204);
+			prepare_body(&fs->htc);
 		}
 	}
 	else if (S_ISLNK(mode)) {
 		// TODO follow link or send redirection ?
 	}
 	else {
-		prepare_answer(htc, 404);
-		prepare_body(htc, NULL);
+		prepare_answer(&fs->htc, 404);
+		prepare_body(&fs->htc);
 	}
 }
 
 static void
-answer_appropriate(struct http_conn *htc)
+answer_appropriate(struct vmod_fsdirector_file_system *fs)
 {
 	unsigned available;
 	char *url;
@@ -203,26 +221,26 @@ answer_appropriate(struct http_conn *htc)
 	char *path;
 	struct stat stat_buf;
 
-	if (strncmp("GET ", htc->ws->s, 4)) {
-		prepare_answer(htc, 405);
-		prepare_body(htc, NULL);
+	if (strncmp("GET ", fs->htc.ws->s, 4)) {
+		prepare_answer(&fs->htc, 405);
+		prepare_body(&fs->htc);
 		return;
 	}
 
-	url_start = &htc->ws->s[4];
+	url_start = &fs->htc.ws->s[4];
 	url_end = strchr(url_start, ' ');
 	url_len = url_end - url_start;
-	url = WS_Alloc(htc->ws, url_len + 1);
+	url = WS_Alloc(fs->htc.ws, url_len + 1);
 	memcpy(url, url_start, url_len + 1);
 	url[url_len] = '\0';
 
 	path = url;
 	if (stat(path, &stat_buf) < 0) {
-		handle_stat_error(htc, errno);
+		handle_stat_error(&fs->htc, errno);
 		return;
 	}
 
-	answer_file(htc, &stat_buf, path);
+	answer_file(fs, &stat_buf, path);
 }
 
 static void *
@@ -230,14 +248,15 @@ server_bgthread(struct worker *wrk, void *priv)
 {
 	struct vmod_fsdirector_file_system *fs;
 	struct sockaddr_storage addr_s;
-	struct http_conn htc;
 	socklen_t len;
+	struct http_conn *htc;
 	int fd;
 	enum htc_status_e htc_status;
 
 	CAST_OBJ_NOTNULL(fs, priv, VMOD_FSDIRECTOR_MAGIC);
 	assert(fs->sock >= 0);
 
+	htc = &fs->htc;
 	fs->wrk = wrk; // XXX hardcoded size for malloc
 	WS_Init(wrk->aws, "fsworkspace-", malloc(4096*1024), 4096*1024);
 
@@ -250,19 +269,19 @@ server_bgthread(struct worker *wrk, void *priv)
 			continue;
 		}
 
-		HTTP1_Init(&htc, wrk->aws, fd, NULL, 2048*1024, 20); // XXX hardcoded
+		HTTP1_Init(htc, wrk->aws, fd, NULL, 2048*1024, 20); // XXX hardcoded
 
-		htc_status = HTTP1_Rx(&htc);
+		htc_status = HTTP1_Rx(htc);
 		switch (htc_status) {
 			case HTTP1_OVERFLOW:
 			case HTTP1_ERROR_EOF:
 			case HTTP1_ALL_WHITESPACE:
 			case HTTP1_NEED_MORE:
-				prepare_answer(&htc, 400);
-				prepare_body(&htc, NULL);
+				prepare_answer(htc, 400);
+				prepare_body(htc);
 				break;
 			case HTTP1_COMPLETE:
-				answer_appropriate(&htc);
+				answer_appropriate(fs);
 				break;
 		}
 
@@ -292,6 +311,22 @@ server_start(struct vmod_fsdirector_file_system *fs)
 	WRK_BgThread(&fs->tp, "fsthread-", server_bgthread, fs);
 }
 
+static magic_t
+load_magic_cookie()
+{
+	magic_t magic_cookie = magic_open(MAGIC_NONE|MAGIC_MIME);
+
+	AN(magic_cookie);
+
+	if (magic_load(magic_cookie, NULL) < 0) {
+		perror("magic_load");
+		magic_close(magic_cookie);
+		magic_cookie = NULL;
+	}
+
+	return magic_cookie;
+}
+
 VCL_VOID
 vmod_file_system__init(struct req *req, struct vmod_fsdirector_file_system **fsp,
     const char *vcl_name, VCL_BACKEND be)
@@ -303,8 +338,10 @@ vmod_file_system__init(struct req *req, struct vmod_fsdirector_file_system **fsp
 	AN(fsp);
 	AN(vcl_name);
 	AZ(*fsp);
+
 	CHECK_OBJ_NOTNULL(be, DIRECTOR_MAGIC);
 	CAST_OBJ_NOTNULL(vs, be->priv, VDI_SIMPLE_MAGIC);
+
 	ALLOC_OBJ(fs, VMOD_FSDIRECTOR_MAGIC);
 	AN(fs);
 	*fsp = fs;
@@ -312,6 +349,9 @@ vmod_file_system__init(struct req *req, struct vmod_fsdirector_file_system **fsp
 
 	fs->vbm = vbit_init(8);
 	AN(fs->vbm);
+
+	fs->magic_cookie = load_magic_cookie();
+	AN(fs->magic_cookie);
 
 	server_start(fs);
 }
@@ -322,7 +362,7 @@ vmod_file_system__fini(struct req *req, struct vmod_fsdirector_file_system **fsp
 	struct vmod_fsdirector_file_system *fs;
 	void *res;
 
-	// XXX It seems like the destructor is not called yet.
+	// XXX It seems that the destructor is not called yet.
 	//     A little reminder then...
 	abort();
 
@@ -337,6 +377,7 @@ vmod_file_system__fini(struct req *req, struct vmod_fsdirector_file_system **fsp
 	AZ(pthread_join(fs->tp, &res));
 	assert(res == PTHREAD_CANCELED);
 
+	magic_close(fs->magic_cookie);
 	free(fs->wrk->aws);
 	FREE_OBJ(fs->wrk);
 	FREE_OBJ(fs);
